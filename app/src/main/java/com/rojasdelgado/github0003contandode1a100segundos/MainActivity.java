d797f8c5a0c54f12ffd1d7de67de3f5ec142e632
rojasredes <31563377+rojasredes@users.noreply.github.com>
2017-09-03 11:45:54 +0200

    inicial

---
+++ app/src/main/java/com/rojasdelgado/github0003contandode1a100segundos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rojasdelgado.github0003contandode1a100segundos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
