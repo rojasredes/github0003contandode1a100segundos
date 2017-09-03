3e3dbdc0f00473961159ce6bc88ba5db1a62214d
rojasredes <31563377+rojasredes@users.noreply.github.com>
2017-09-03 11:58:22 +0200

    onStart modificando etiqueta.setText a cada bucle, no se ve que cambie la etiqueta, solo se ve al final "Terminamos"

--- app/src/main/java/com/rojasdelgado/github0003contandode1a100segundos/MainActivity.java
+++ app/src/main/java/com/rojasdelgado/github0003contandode1a100segundos/MainActivity.java
@@ -2,12 +2,39 @@ package com.rojasdelgado.github0003contandode1a100segundos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int contarhasta=5; //cuento hasta 5, porque contar hasta 100 me hace esperar 100 segundos sin ver nada
+    TextView etiqueta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etiqueta=(TextView)findViewById(R.id.etiqueta);
+        etiqueta.setText("Vamos a contar de 1 a 100");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        etiqueta.setText("Empezamos...");
+
+        for(int i=1;i<=contarhasta;i++)
+        {
+            etiqueta.setText(Integer.valueOf(i).toString()); //esto no se ve
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        etiqueta.setText("Terminamos.");
     }
 }
